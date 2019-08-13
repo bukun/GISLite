32b6e134701ddd650863d2596c112311fd567f77
bukun <bukun@osgeo.cn>
2019-08-13 21:11:26 +0800

    modify the slug for html url.

--- deploy.py
+++ deploy.py
@@ -6,18 +6,19 @@ from fabric import Connection
 from cfg import mach, SITE_WS, GIS_BASE_REMOTE
 
 
-
 def main():
     print("{}@{}".format(mach['u'], mach['h']))
-    c = Connection("{}@{}".format(mach['u'], mach['h']),
-                   port=11022,
-                   connect_kwargs={"password": mach['p']})
+    c = Connection(
+        "{}@{}".format(mach['u'], mach['h']),
+        port=11022,
+        connect_kwargs={"password": mach['p']}
+    )
 
     c.run('sudo chown -R bk {}/dist_site'.format(SITE_WS))
     c.run('sudo chown -R bk {}'.format(GIS_BASE_REMOTE))
 
     with c.cd(SITE_WS):
-        c.run('git pull')
+        # c.run('git pull')
         c.run('python3 build_gislite.py')
 
     c.run('sudo chown -R www-data.www-data {}/dist_site'.format(SITE_WS))

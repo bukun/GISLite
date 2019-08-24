0293dd4a9920f689960d23375092d3349a82c16b
bukun <bukun@osgeo.cn>
2019-08-24 20:18:02 +0800

    mark.

--- gislite/build_site.py
+++ gislite/build_site.py
@@ -309,7 +309,7 @@ def fetch_structure():
         if the_dir in ['symbols', 'fonts']:
             continue
         xx_dir = os.path.join(src_ws, the_dir)
-        if os.path.isdir(xx_dir):
+        if os.path.isdir(xx_dir) and 'maplet' in xx_dir :
             pass
         else:
             continue

7095743f02c1a3da552d37cd44efe213b6dd9cc8
bukun <bukun@osgeo.cn>
2019-08-24 19:45:46 +0800

    mark.

--- cfg_demo.py
+++ cfg_demo.py
@@ -1,9 +1,12 @@
-
-# The mapproxy server.
+# 服务器端配置 #
 TILE_SVR = '192.168.56.1:8011'
-
 # Path for GIS data.
 GIS_BASE = '/home/bk/opt/geodata'
 
+## 客户端配置，声明服务器的情况
+SITE_WS = '/home/bk/github/GISLite'
+machines = {
+    'aliyun3': {'u': 'bk', 'h': '192.168.56.1', 'p': ''},
+}
 
-
+mach = machines['aliyun3']
--- deploy.py
+++ deploy.py
@@ -1,31 +1,11 @@
-'''
-发布 WebGIS.
-'''
-from fabric import Connection
+import subprocess
 
-from cfg import mach, SITE_WS, GIS_BASE_REMOTE
+from cfg import SITE_WS, GIS_BASE
 
-
-def main():
-    print("{}@{}".format(mach['u'], mach['h']))
-    c = Connection(
-        "{}@{}".format(mach['u'], mach['h']),
-        port=11022,
-        connect_kwargs={"password": mach['p']}
-    )
-
-    c.run('sudo chown -R bk {}/dist_site'.format(SITE_WS))
-    c.run('sudo chown -R bk {}'.format(GIS_BASE_REMOTE))
-
-    with c.cd(SITE_WS):
-        # c.run('git reset --hard')
-        # c.run('git pull')
-        c.run('python3 build_gislite.py')
-
-    c.run('sudo chown -R www-data.www-data {}/dist_site'.format(SITE_WS))
-    c.run('sudo chown -R www-data.www-data {}'.format(GIS_BASE_REMOTE))
-    c.run('sudo supervisorctl restart gislite')
-
-
-if __name__ == '__main__':
-    main()
+subprocess.run('sudo chown -R bk {}/dist_site'.format(SITE_WS), shell=True)
+subprocess.run('sudo chown -R bk {}'.format(GIS_BASE), shell=True)
+subprocess.run('cd {} && python3 build_gislite.py'.format(SITE_WS), shell=True)
+# subprocess.run()
+subprocess.run('sudo chown -R www-data.www-data {}/dist_site'.format(SITE_WS), shell=True)
+subprocess.run('sudo chown -R www-data.www-data {}'.format(GIS_BASE), shell=True)
+subprocess.run('sudo supervisorctl restart gislite', shell=True)

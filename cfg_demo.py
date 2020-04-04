92891ddd4c1e8891770189620abd1ed90c96a9f9
bukun <bukun@osgeo.cn>
2020-04-04 15:08:00 +0800

    add demo data.

--- cfg_demo.py
+++ cfg_demo.py
@@ -2,7 +2,7 @@
 # 服务器端配置 #
 TILE_SVR = '192.168.56.1:8011'
 # Path for GIS data.
-GIS_BASE = '/home/bk/opt/geodata'
+GIS_BASE = '/home/bk/opt/geodemo'
 
 ## 服务器的工作空间
 SITE_WS = '/home/bk/github/GISLite'

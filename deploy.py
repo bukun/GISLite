121c025f7b07655b0162274be455a9b4ac82fbee
bukun <bukun@osgeo.cn>
2020-04-06 09:06:14 +0800

    clean the codes.

--- deploy.py
+++ deploy.py
@@ -9,7 +9,7 @@ from cfg import SITE_WS, GIS_BASE, USER, DAMONS
 subprocess.run('sudo chown -R {} {}/dist_site/*'.format(USER, SITE_WS), shell=True)
 subprocess.run('sudo chown -R {} {}/dist_site'.format(USER, SITE_WS), shell=True)
 subprocess.run('sudo chown -R {} {}/*'.format(USER, GIS_BASE), shell=True)
-subprocess.run('cd {} && python3 build_gislite.py'.format(SITE_WS), shell=True)
+subprocess.run('cd {} && python3 gisliter.py'.format(SITE_WS), shell=True)
 subprocess.run('sudo chown -R www-data.www-data {}/dist_site'.format(SITE_WS), shell=True)
 subprocess.run('sudo chown -R www-data.www-data {}'.format(GIS_BASE), shell=True)
 
